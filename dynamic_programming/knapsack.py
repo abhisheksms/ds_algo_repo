b3c5924b14188fcc5f872e5c37f8dd25d7deb267
Abhishek S <abhisheksms97@gmail.com>
2021-05-26 14:23:44 +0530

    Update knapsack.py

--- dynamic_programming/knapsack.py
+++ dynamic_programming/knapsack.py
@@ -23,3 +23,29 @@ def knapsack(wt, val, W, n):
 
 
 print(knapsack(wt, val, W, n))
+
+##################################################################################
+# iterative approach
+
+from itertools import product
+
+# add extra buffers
+dp = [[0]*10 for _ in range(5)]
+def knapsack(wt, val, W, n):
+    """
+    i : n
+    j : w
+    """
+    for i, j in product(range(n+1), range(W+1)):
+        if i==0 or j==0:
+            dp[i][j] = 0
+
+    for i, j in product(range(1, n+1), range(1, W+1)):
+        if wt[i-1] <= j:
+            dp[i][j] = max(val[i-1] + dp[i-1][j-wt[i-1]],
+                           dp[i-1][j])
+        else:
+            dp[i][j] = dp[i-1][j]
+
+    print(dp)
+    return dp[n][W]
