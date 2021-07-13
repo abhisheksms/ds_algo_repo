570504b1e10b0044c0830fb47ad9f2bf5006e1fc
Abhishek S <abhisheksms97@gmail.com>
2021-07-13 21:14:46 +0530

    Knapsack with and without repetition

--- dynamic_programming/knapsack.py
+++ dynamic_programming/knapsack.py
@@ -1,51 +1,50 @@
-# initializing a 2d matrinx of W x n
+def knapsack_with_repetion(wt, val, W, n):
+    """
+    Unbounded knapsack
+    """
+    # knapsack array of length W+1
+    kw = [0]*(W+1)
+
+    # kw[0] : knapsack with weight 0
+    for w in range(1, W+1):
+        for i in range(n):
+            if wt[i] <= w:
+
+                # k(w) = max(k(w - w[i]) + v[i])
+                kw[w] = max(kw[w], kw[w-wt[i]] + val[i])
+
+    print(kw)
+    return kw[-1]
+
+#######################
+# W = 100
+# val = [10, 30, 20]
+# wt = [5, 10, 15]
+# n = len(val)
+# print(knapsack_with_repetion(wt, val, W, n))
+
+
+from itertools import product
+def knapsack_without_repetion(wt, val, W, n):
+    dp = [[0]*(n+1) for _ in range(W+1)]
+
+    i : W(wt)
+    j : n(val)
+    for i,j in product(range(W+1), range(n+1)):
+        # if not (i and j)
+        if i==0 or j==0:
+            dp[i][j] = 0
+        elif wt[j-1]<=i:
+            dp[i][j] = max(dp[i][j-1], val[j-1]+dp[i-wt[j-1]][j-1])
+        else:
+            dp[i][j] = dp[i][j-1]
+
+    print(dp)  
+    return dp[W][n]
+
+
 wt = [1, 3, 4, 5]
 val = [1, 4, 5, 7]
 W = 9
 n = 4
-
-# add extra buffers
-dp = [[-1]*5 for _ in range(10)]
-def knapsack(wt, val, W, n):
-    if n == 0 or W == 0:
-        return 0
-
-    if dp[W][n] != -1:
-        return dp[W][n]
-
-    if wt[n-1] <= W:
-        dp[W][n] = max(val[n-1] + knapsack(wt, val, W-wt[n-1], n-1), 
-                              knapsack(wt, val, W, n-1))
-    else:
-        dp[W][n] = knapsack(wt, val, W, n-1)
-
-    return dp[W][n]
-
-
-print(knapsack(wt, val, W, n))
-
-##################################################################################
-# iterative approach
-
-from itertools import product
-
-# add extra buffers
-dp = [[0]*10 for _ in range(5)]
-def knapsack(wt, val, W, n):
-    """
-    i : n
-    j : w
-    """
-    for i, j in product(range(n+1), range(W+1)):
-        if i==0 or j==0:
-            dp[i][j] = 0
-
-    for i, j in product(range(1, n+1), range(1, W+1)):
-        if wt[i-1] <= j:
-            dp[i][j] = max(val[i-1] + dp[i-1][j-wt[i-1]],
-                           dp[i-1][j])
-        else:
-            dp[i][j] = dp[i-1][j]
-
-    print(dp)
-    return dp[n][W]
+print(knapsack_without_repetion(wt, val, W, n))
